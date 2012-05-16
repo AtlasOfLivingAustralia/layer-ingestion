22067e667d06018faadf013cc23ee7bff6102e6d
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com>
2012-05-16 02:51:40 +0000

    further work on automation of migration for dev to prod.
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layer-ingestion@1939 73def43e-0e3b-11df-a5af-b38b39d06e06

--- migrate_contextual.sh
+++ migrate_contextual.sh
@@ -2,6 +2,7 @@
 export SSH_USERNAME=fle13g
 export LAYER_ID=990
 export LAYER_SHORT_NAME=alwc4
+export LAYER_DESCRIPTION=
 
 export DEV_SERVER=ala-devmaps.vm.csiro.au
 export SHAPE_DIR="/data/ala/data/layers/ready/shape"
@@ -20,7 +21,7 @@ export GEOSERVERBASEURL="http://localhost:8082/geoserver"
 export GEOSERVERUSERNAME="admin"
 export GEOSERVERPASSWORD="at1as0f0z"
 
-export JAVA_CLASSPATH="./layer-ingestion-1.0-SNAPSHOT:./lib/*"
+export JAVA_CLASSPATH="./layer-ingestion-1.0-SNAPSHOT.jar:./lib/*"
 
 echo "Copy shape files from dev server" \
 && scp ${SSH_USERNAME}@${DEV_SERVER}:${SHAPE_DIR}/${LAYER_SHORT_NAME}.* ${SHAPE_DIR} \
@@ -29,22 +30,14 @@ echo "Copy shape files from dev server" \
 && echo "Change ownership of copied shape files" \
 && chown tomcat:10 ${SHAPE_DIR}/${LAYER_SHORT_NAME}.* \
 && echo "copy database entries from dev database" \
-&& java -Xmx10G -cp "${JAVA_CLASSPATH}" MigrateLayerDatabaseEntries "${LAYER_ID}" "${DEV_DB_JDBC_URL}" "${DEV_DB_USERNAME}" "${DEV_DB_PASSWORD}" "${PROD_DB_JDBC_URL}" "${PROD_DB_USERNAME}" "${PROD_DB_PASSWORD}" \
+&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.MigrateLayerDatabaseEntries "${LAYER_ID}" "${DEV_DB_JDBC_URL}" "${DEV_DB_USERNAME}" "${DEV_DB_PASSWORD}" "${PROD_DB_JDBC_URL}" "${PROD_DB_USERNAME}" "${PROD_DB_PASSWORD}" \
 && echo "Create database table from shapefile" \
-&& shp2pgsql -I -s 4326 "${SHAPE_DIR}/${LAYER_SHORT_NAME}.shp" "${LAYER_ID}" | psql -h "${PROD_DB_HOST}" -d "${PROD_DB_NAME}" -U "${PROD_DB_USERNAME}"
+&& shp2pgsql -I -s 4326 "${SHAPE_DIR}/${LAYER_SHORT_NAME}.shp" "${LAYER_ID}" | psql -h "${PROD_DB_HOST}" -d "${PROD_DB_NAME}" -U "${PROD_DB_USERNAME}" \
 && echo "Create objects from layer" \
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.contextual.ContextualObjectCreator "${LAYER_ID}" "${PROD_DB_USERNAME}" "${PROD_DB_PASSWORD}" "${PROD_DB_JDBC_URL}" \
 && echo "Load layer in geoserver" \
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.PostgisTableGeoserverLoader "${GEOSERVERBASEURL}" "${GEOSERVERUSERNAME}" "${GEOSERVERPASSWORD}" "${LAYER_ID}" "${LAYER_SHORT_NAME}" "${LAYER_DESCRIPTION}"
 
-
-#&& echo "Download sld file from dev db" \
-#&& wget http://spatial-dev.ala.org.au/geoserver/rest/styles/${LAYER_ID}_style.sld \
-
-
-
-# Analysis files
-# Tabulation data
 # Layer thumbnails
 
 
--- migrate_contextual_with_sld.sh
+++ migrate_contextual_with_sld.sh
@@ -2,10 +2,10 @@
 export SSH_USERNAME=fle13g
 export LAYER_ID=990
 export LAYER_SHORT_NAME=alwc4
+export LAYER_DESCRIPTION=
 
 export DEV_SERVER=ala-devmaps.vm.csiro.au
-export DIVA_DIR="/data/ala/data/layers/ready/diva"
-export GEOTIFF_DIR="/data/ala/data/layers/ready/geotiff"
+export SHAPE_DIR="/data/ala/data/layers/ready/shape"
 
 export DEV_DB_JDBC_URL="jdbc:postgresql://ala-devmaps-db.vm.csiro.au:5432/layersdb"
 export DEV_DB_USERNAME=postgres
@@ -23,26 +23,24 @@ export GEOSERVERPASSWORD="at1as0f0z"
 
 export JAVA_CLASSPATH="./layer-ingestion-1.0-SNAPSHOT.jar:./lib/*"
 
-echo "Copy diva files from dev server" \
-&& scp ${SSH_USERNAME}@${DEV_SERVER}:${DIVA_DIR}/${LAYER_SHORT_NAME}.* ${DIVA_DIR} \
-&& echo "Change mode of copied shapefile" \
-&& chmod 777 ${DIVA_DIR}/${LAYER_SHORT_NAME}.* \
-&& echo "Change ownership of copied shapefile" \
-&& chown tomcat:10 ${DIVA_DIR}/${LAYER_SHORT_NAME}.* \
-&& echo "Copy geotiff files from dev server" \
-&& scp ${SSH_USERNAME}@${DEV_SERVER}:${GEOTIFF_DIR}/${LAYER_SHORT_NAME}.tif ${GEOTIFF_DIR} \
-&& echo "Change mode of copied geotiff" \
-&& chmod 777 ${GEOTIFF_DIR}/${LAYER_SHORT_NAME}.tif \
-&& echo "Change ownership of copied geotiff" \
-&& chown tomcat:10 ${GEOTIFF_DIR}/${LAYER_SHORT_NAME}.tif \
+echo "Copy shape files from dev server" \
+&& scp ${SSH_USERNAME}@${DEV_SERVER}:${SHAPE_DIR}/${LAYER_SHORT_NAME}.* ${SHAPE_DIR} \
+&& echo "Change mode of copied shape files" \
+&& chmod 777 ${SHAPE_DIR}/${LAYER_SHORT_NAME}.* \
+&& echo "Change ownership of copied shape files" \
+&& chown tomcat:10 ${SHAPE_DIR}/${LAYER_SHORT_NAME}.* \
 && echo "copy database entries from dev database" \
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.MigrateLayerDatabaseEntries "${LAYER_ID}" "${DEV_DB_JDBC_URL}" "${DEV_DB_USERNAME}" "${DEV_DB_PASSWORD}" "${PROD_DB_JDBC_URL}" "${PROD_DB_USERNAME}" "${PROD_DB_PASSWORD}" \
+&& echo "Create database table from shapefile" \
+&& shp2pgsql -I -s 4326 "${SHAPE_DIR}/${LAYER_SHORT_NAME}.shp" "${LAYER_ID}" | psql -h "${PROD_DB_HOST}" -d "${PROD_DB_NAME}" -U "${PROD_DB_USERNAME}" \
+&& echo "Create objects from layer" \
+&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.contextual.ContextualObjectCreator "${LAYER_ID}" "${PROD_DB_USERNAME}" "${PROD_DB_PASSWORD}" "${PROD_DB_JDBC_URL}" \
 && echo "copy sld legend file from dev geoserver to current directory" \
 && wget http://spatial-dev.ala.org.au/geoserver/rest/styles/${LAYER_SHORT_NAME}_style.sld \
-&& echo "Load layer into geoserver" \
-&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.GeotiffGeoserverLoader "${LAYER_NAME}" "${GEOTIFF_DIR}/${LAYER_NAME}.tif" "./${LAYER_SHORT_NAME}_style.sld" "${GEOSERVERBASEURL}" "${GEOSERVERUSERNAME}" "${GEOSERVERPASSWORD}"
+&& echo "Load layer in geoserver" \
+&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.PostgisTableGeoserverLoader "${GEOSERVERBASEURL}" "${GEOSERVERUSERNAME}" "${GEOSERVERPASSWORD}" "${LAYER_ID}" "${LAYER_SHORT_NAME}" "${LAYER_DESCRIPTION}" "./${LAYER_SHORT_NAME}_style.sld" \
 && echo "delete temporary sld file" \
-&& rm "./${LAYER_SHORT_NAME}_style.sld"
+&& echo rm "./${LAYER_SHORT_NAME}_style.sld"
 
 # Layer thumbnails
 
