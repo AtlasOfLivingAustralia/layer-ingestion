34ec8b22ae81ac0214b70840927c6b0034b4341d
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com>
2012-05-22 03:53:52 +0000

    further automation work
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layer-ingestion@1962 73def43e-0e3b-11df-a5af-b38b39d06e06

--- contextual_load_GridClassesBuilder.sh
+++ contextual_load_GridClassesBuilder.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # Loads a contextual layer from raw .adf data, using the GridClassesBuilder to generate class data which is
 # read from disk
-# NOTE: The following 5 variables need to be modified for each new layer
+# NOTE: The following 4 variables need to be modified for each new layer
 export LAYER_ID=111
 export LAYER_NAME="ga_bath_rocky" 
 export LAYER_DESCRIPTION=
-export UNITS="%" 
 export ADF_HEADER_FILE="/data/ala/data/layers/raw/ga_bath_rocky/hdr.adf"
 
 export DB_USERNAME="postgres"
 export DB_PASSWORD="Passw0rd"
-export DB_JDBC_URL="jdbc:postgresql://ala-devmaps-db.vm.csiro.au:5432/layersdb"
+export DB_JDBC_URL="jdbc:postgresql://localhost:5432/layersdb"
 
-export GEOSERVER_BASE_URL="http://localhost:8082/geoserver"
+export GEOSERVER_BASE_URL="http://localhost:8080/geoserver"
 export GEOSERVER_USERNAME="admin"
 export GEOSERVER_PASSWORD="at1as0f0z" 
 
@@ -31,8 +30,6 @@ echo "create process directory"
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" org.ala.layers.util.Bil2diva "${PROCESS_DIR}/${LAYER_NAME}/${LAYER_NAME}" "${SHAPE_DIVA_DIR}/${LAYER_NAME}" "${UNITS}" \
 && echo "Run GridClassBuilder tool" \
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" org.ala.layers.grid.GridClassBuilder "${SHAPE_DIVA_DIR}/${LAYER_NAME}" \
-&& echo "Copy generated .sld legend file to legend directory" \
-&& 
 && echo "Convert diva for polygons into bil"
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" org.ala.layers.util.Diva2bil "${SHAPE_DIVA_DIR}/${LAYER_NAME}/polygons" "${PROCESS_DIR}/${LAYER_NAME}/${LAYER_NAME}" \
 && echo "Convert polygons bil into geotiff"
@@ -40,4 +37,4 @@ echo "create process directory"
 && echo "Creating layer and fields table entries for layer" \
 && java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.contextual.ContextualFromGridDatabaseLoader "${LAYER_ID}" "${LAYER_NAME}" "${LAYER_DESCRIPTION}" "${UNITS}" "${DIVA_DIR}/${LAYER_NAME}.grd" "${DB_USERNAME}" "${DB_PASSWORD}" "${DB_JDBC_URL}" \
 && echo "Load layer into geoserver" \
-&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.GeotiffGeoserverLoader "${LAYER_NAME}" "${GEOTIFF_DIR}/${LAYER_NAME}.tif" "${LEGEND_DIR}/${LAYER_NAME}.sld" "${GEOSERVER_BASE_URL}" "${GEOSERVER_USERNAME}" "${GEOSERVER_PASSWORD}"
+&& java -Xmx10G -cp "${JAVA_CLASSPATH}" au.org.ala.layers.ingestion.GeotiffGeoserverLoader "${LAYER_NAME}" "${GEOTIFF_DIR}/${LAYER_NAME}.tif" "${SHAPE_DIVA_DIR}/${LAYER_NAME}/polygons.sld" "${GEOSERVER_BASE_URL}" "${GEOSERVER_USERNAME}" "${GEOSERVER_PASSWORD}"
