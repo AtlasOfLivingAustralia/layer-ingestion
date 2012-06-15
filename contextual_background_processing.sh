a482dac6f4b73b7c0cba6759bea53cfadd54ee48
chris.flemming.ala@gmail.com <chris.flemming.ala@gmail.com>
2012-06-15 02:12:07 +0000

    minor fix to contextual background processing script
    git-svn-id: http://alageospatialportal.googlecode.com/svn/trunk/layer-ingestion@2110 73def43e-0e3b-11df-a5af-b38b39d06e06

--- contextual_background_processing.sh
+++ contextual_background_processing.sh
@@ -12,9 +12,14 @@ export DB_PASSWORD="postgres"
 echo "Preparing contextual layers for analysis" \
 && java -Xmx20G -DANALYSIS_RESOLUTIONS=0.5,0.01 -cp "${JAVA_CLASSPATH}" org.ala.layers.util.AnalysisLayerUtil auto shapes \
 && echo "Generating tabulation for contextual layers" \
+&& echo "tabulation step 1" \
 && java -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 1 \
-&& -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 3 \
-&& -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 5 "${PATH_TO_RECORDS_CSV}" \
-&& -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 6 "${PATH_TO_RECORDS_CSV}" \
-&& -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 4
+&& echo "tabulation step 3" \
+&& java -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 3 \
+&& echo "tabulation step 5" \
+&& java -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 5 "${PATH_TO_RECORDS_CSV}" \
+&& echo "tabulation step 6" \
+&& java -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 6 "${PATH_TO_RECORDS_CSV}" \
+&& echo "tabulation step 4" \
+&& java -Xmx20G -cp "${JAVA_CLASSPATH}" org.ala.layers.tabulation.TabulationGenerator 4 "${DB_JDBC_URL}" "${DB_USERNAME}" "${DB_PASSWORD}" 4
 # TODO layer thumbnails
